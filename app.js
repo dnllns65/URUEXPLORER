bc78e61b5d978c1764390d21aa0cc667f26eabb3
dnllns65 <dnllns65@gmail.com>
2026-08-17 11:48:50 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -989,7 +989,11 @@ function applyTranslations() {
     document.querySelectorAll('[data-i18n]').forEach(el => {
         const key = el.getAttribute('data-i18n');
         if (dict[key]) {
-            el.textContent = dict[key];
+            if (key === 'guide_steps') {
+                el.innerHTML = dict[key];
+            } else {
+                el.textContent = dict[key];
+            }
         }
     });
 
